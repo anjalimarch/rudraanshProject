69148e33308bd57620115fa6d594eb51e9610a40
Anjali <anjalimarch24@gmail.com>
2018-01-26 22:35:09 +0530

    test

---
+++ TrainingJavaPrograms/src/testInstanceOfoperator/A.java
@@ -0,0 +1,20 @@
+package testInstanceOfoperator;
+
+public class A implements Printable{
+
+	public void a(){
+		
+		System.out.println("a method");
+	}
+
+	@Override
+public	void c() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+public void a1(){
+		
+		System.out.println("a method");
+	}
+}
