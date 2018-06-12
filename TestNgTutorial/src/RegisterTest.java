8f2712ea75750e11cb8046bd334aad03d0e5ac57
Anjali <anjalimarch24@gmail.com>
2018-06-12 11:19:08 +0530

    Testng Project

---
+++ TestNgTutorial/src/RegisterTest.java
@@ -0,0 +1,23 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class RegisterTest {
+
+	@Test
+	public void registerTest(){
+		
+		String expectedTitle="xxxxxxx";
+		String actualTitle="yyyyyyy";
+		
+		Assert.assertEquals(actualTitle, expectedTitle);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
