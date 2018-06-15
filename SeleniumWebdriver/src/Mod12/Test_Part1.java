4478191f2091f3990aab97f62ab4a6e185304ece
Anjali <anjalimarch24@gmail.com>
2018-06-15 17:51:44 +0530

    Commiting TestPart1

---
+++ SeleniumWebdriver/src/Mod12/Test_Part1.java
@@ -0,0 +1,45 @@
+package Mod12;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Test_Part1 {
+	public static String browserNamemozilla = "Mozilla";
+	public static String browserNameChrome = "Chrome";
+
+	public static String Url = "https://mail.rediff.com/cgi-bin/login.cgi";
+
+	public static WebDriver driver;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		openBrowser(browserNameChrome);
+	}
+
+	public static void openBrowser(String browsertorun) {
+
+		if (browsertorun.equals(browserNamemozilla)) {
+			System.setProperty("webdriver.gecko.driver", "D:\\SeleniumThreeJars\\GeckoDriver\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		} else if (browsertorun.equals(browserNameChrome)) {
+
+			System.setProperty("webdriver.chrome.driver", "D:\\SeleniumThreeJars\\chromedriver\\chromedriver.exe");
+			driver = new ChromeDriver();
+
+		} /*
+			 * else if(browsertorun.equals(browserName)){
+			 * 
+			 * System.setProperty("webdriver.gecko.driver",
+			 * "D:\\SeleniumThreeJars\\GeckoDriver\\geckodriver.exe");
+			 * driver=new InternetExplorerDriver();
+			 * 
+			 * }
+			 */
+		driver.get(Url);
+		System.out.println("Title of the page ---" + driver.getTitle());
+		driver.quit();
+
+	}
+
+}
